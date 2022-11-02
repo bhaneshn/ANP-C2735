39f69de2157db203b76606dded1091d69aa7a9a0
Bhanesh Nawale <102051002+bhaneshn@users.noreply.github.com>
2022-11-02 13:41:22 +0530

    forrrrr

---
+++ ForExapmle/src/Close/org/close.java
@@ -0,0 +1,12 @@
+package Close.org;
+
+public class close {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("joll");
+
+	}
+
+}
--- Practice/src/org/practice/Pattern.java
+++ Practice/src/org/practice/Pattern.java
@@ -1,5 +1,49 @@
 package org.practice;
 
+import java.util.Scanner;
+
 public class Pattern {
 
+	public static void main(String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the number: ");
+	
+	int n=scan.nextInt();
+//		
+//		for(int i=1;i<=n;i++) {
+//			
+//			for(int j=1;j<=i;j++) {
+//				System.out.print("* ");
+//			}
+//			System.out.println();
+//		}
+		
+//	for(int i=1;i<=n;i++) {
+//		
+//		for(int j=1;j<i;j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//	}
+	
+//       for(int i=1;i<=n;i++) {
+//		
+//		  for(int j=1;j<i;j++) {
+//			  
+//			System.out.print(i+" ");
+//		}
+//		System.out.println();
+//	}
+	int no=0;
+	for(int i=1;i<=n;i++) {
+		
+		for(int j=1;j<i;j++) {
+			no++;
+			System.out.print(no+" ");
+		}
+		System.out.println();
+	}
+	
+	}
 }
